026485582dd476287412eab0e5747dcfa97bddfd
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2023-12-16 23:41:18 +0500

    Update servicesModel.js

--- route/services/servicesModel.js
+++ route/services/servicesModel.js
@@ -12,6 +12,10 @@ const servicesSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    icon: {
+      type: String,
+      required: true,
+    },
    
   },
   { timestamps: true }
